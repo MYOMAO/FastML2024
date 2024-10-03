e0e73febb4b796b5cb3902e79295f0839d4df38f
MYOMAO <zshi2@mail.ccsf.edu>
2024-10-03 16:20:49 -0400

    Updated with Install Directory

--- Link.sh
+++ Link.sh
@@ -12,7 +12,8 @@ source /opt/sphenix/core/bin/sphenix_setup.csh -n ana.376
 #setenv ROOT_INCLUDE_PATH /sphenix/user/zshi/EvtGenPullRequest/macros/common:$ROOT_INCLUDE_PATH
 
 #setenv MYINSTALL /sphenix/user/xyu3/PublicBuild/install/
-setenv MYINSTALL /sphenix/user/xyu3/ForZhaozhong/PublicBuild/install/
+#setenv MYINSTALL /sphenix/user/xyu3/ForZhaozhong/PublicBuild/install/
+setenv MYINSTALL $PWD/install/
 setenv LD_LIBRARY_PATH $MYINSTALL/lib:$LD_LIBRARY_PATH
 set path = ( $MYINSTALL/bin $path )
 
